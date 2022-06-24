de94c11e1ccd5630702a01837e267598f097928b
Shumakov <nikk8721@gmail.com>
2022-06-24 12:23:40 +0500

    Chetnoe_chislo

---
+++ Chetnoe/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA % 2 == 1)
+{
+    Console.WriteLine("Число нечетное");
+}
+else 
+{
+    Console.WriteLine("Число четное");
+}
