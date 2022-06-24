5bfe18c21153a10cf82ef075fa5ebcad1cac2d6f
Shumakov <nikk8721@gmail.com>
2022-06-24 11:19:28 +0500

    MAX-MIN_Chislo

---
+++ MAX-MIN/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число A");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB)
+{
+    Console.Write("max = ");
+    Console.WriteLine(numberA);
+    Console.Write("min = ");
+    Console.WriteLine(numberB);
+}
+else
+{
+    Console.Write("min = ");
+    Console.WriteLine(numberA);
+    Console.Write("max = ");
+    Console.WriteLine(numberB);
+}
+
+
