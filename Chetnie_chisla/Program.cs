c61c76094be63a6c58179b5c9ad38f07eff68f17
Shumakov <nikk8721@gmail.com>
2022-06-24 14:46:14 +0500

    Correcting Error in Programm

--- Chetnie_chisla/Program.cs
+++ Chetnie_chisla/Program.cs
@@ -1,12 +1,14 @@
 ﻿Console.WriteLine("Введите число ");
 int numberA = Convert.ToInt32(Console.ReadLine());
+int numberR = 0;
 if (numberA % 2 == 1)
 {
     numberA = numberA - 1;
 }
-while (numberA > 2)
+while (numberA > 0)
 {
+    numberR = numberR + 2;
+    Console.WriteLine(numberR);
     numberA = numberA - 2;
-    Console.WriteLine(numberA);
 }
 
