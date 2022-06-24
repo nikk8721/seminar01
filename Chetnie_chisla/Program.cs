43f3c50ae3c42f5f49f92ecb59b20f8a18b3fe76
Shumakov <nikk8721@gmail.com>
2022-06-24 15:52:00 +0500

    Correcting  Programm

--- Chetnie_chisla/Program.cs
+++ Chetnie_chisla/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.WriteLine("Введите число ");
 int numberA = Convert.ToInt32(Console.ReadLine());
-int numberR = 0;
+int numberR = 0; 
 if (numberA % 2 == 1)
 {
     numberA = numberA - 1;
@@ -8,7 +8,8 @@ if (numberA % 2 == 1)
 while (numberA > 0)
 {
     numberR = numberR + 2;
-    Console.WriteLine(numberR);
+    Console.Write(numberR);
+    Console.Write(",");
     numberA = numberA - 2;
 }
 
