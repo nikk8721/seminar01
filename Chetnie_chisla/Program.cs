c6833e01d58c7defb44d299c792d0be1f2fa09ac
Shumakov <nikk8721@gmail.com>
2022-06-24 13:26:06 +0500

    Seies of even numbers

---
+++ Chetnie_chisla/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA % 2 == 1)
+{
+    numberA = numberA - 1;
+}
+while (numberA > 2)
+{
+    numberA = numberA - 2;
+    Console.WriteLine(numberA);
+}
+
