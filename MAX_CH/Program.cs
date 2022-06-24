6728707605e6ec925d232b9a4ab7003a7aba7d54
Shumakov <nikk8721@gmail.com>
2022-06-24 12:11:31 +0500

    MAX_Chislo

---
+++ MAX_CH/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите число A");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число С");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int maxCh = numberA;
+if (numberA > numberB)
+{
+    maxCh = numberA;
+}
+else
+{
+    maxCh = numberB;
+}
+if (numberA > numberC)
+{
+    maxCh = numberA;
+}
+else
+{
+    maxCh = numberC;
+}
+if (numberA > numberC)
+{
+    maxCh = numberA;
+}
+else
+{
+    maxCh = numberC;
+}
+Console.Write("Максимальное число = ");
+Console.WriteLine(maxCh);
+
